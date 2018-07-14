702441f6c6fc751a91ae1ff0019f0f8191dd29ea
Demon Singur <demonsingur@gmail.com>
2018-07-14 14:37:51 +0000

    msm8996-common: Do not build libxml2
    * They are compiled as dependencies of ipacm.
    
    Change-Id: I72076113083308e26573289923f5d3c00a8a6b44

--- msm8996.mk
+++ msm8996.mk
@@ -336,8 +336,7 @@ PRODUCT_PACKAGES += \
     android.hardware.radio@1.4.vendor \
     android.hardware.radio.config@1.0 \
     android.hardware.radio.config@1.0.vendor \
-    librmnetctl \
-    libxml2
+    librmnetctl
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
