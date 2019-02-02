fb06c7ee6e0d3f290d5bae7104bbdfb2bae0935d
Davide Garberi <dade.garberi@gmail.com>
2019-02-02 11:42:45 +0100

    msm8996-common: Enable system-as-root
    Change-Id: Idd76dec60c9a7a010372b28b29c7ff9aa1da08e0

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -193,6 +193,10 @@ TARGET_USES_OLD_MNC_FORMAT := true
 # Security patch level
 VENDOR_SECURITY_PATCH := 2018-11-05
 
+# System as root
+BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
+BOARD_KERNEL_CMDLINE += skip_initramfs rootwait ro init=/init root=/dev/dm-0
+
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
 
