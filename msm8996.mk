3c0ace7ba12322760b5bcfb8c9fb6c8fb94e6cbb
Jiyong Park <jiyong@google.com>
2019-04-15 14:19:03 +0900

    msm8996-common: Add updatable_apex.mk
    The product abstracts configs that need to be set to support updating of APEXes.
    
    Change-Id: Id5aa944ceba6a6846308f1c18a08af8b3bb38589

--- msm8996.mk
+++ msm8996.mk
@@ -23,6 +23,9 @@
 #
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/zuk/msm8996-common/msm8996-common-vendor.mk)
 
